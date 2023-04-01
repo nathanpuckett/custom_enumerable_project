317202d89e5376c0b3a5dddab8ab2ccfe2ef537e
nathanpuckett <nathanfpuckett@gmail.com>
2023-03-31 20:08:03 -0500

    Complete my_each_with_index method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,14 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index(&block)
+    for i in 0...self.length do
+      block.call(self[i], i)
+    end
+
+    self
+  end
+
+  
 end
 
 # You will first have to define my_each
